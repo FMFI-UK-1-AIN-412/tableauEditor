6ef1f3ced526fa2caf9fe9e694db830e7c0a130e
Jozef Šiška <siska@ii.fmph.uniba.sk>
2017-04-05 21:18:20 +0200

    Initial Formula implementation and parser

---
+++ src/Formula.elm
@@ -0,0 +1,82 @@
+module Formula exposing (..)
+
+import Char
+import Set
+import Parser exposing
+  (
+    Parser, (|.), (|=), succeed, symbol,
+    float, ignore, zeroOrMore, oneOf, lazy,
+    keyword, delayedCommitMap, end, oneOrMore
+  )
+import Parser.LanguageKit exposing (variable, sequence, whitespace)
+
+
+type Formula
+  = Atom String
+  | FF
+  | FT
+  | Neg Formula
+  | Disj Formula Formula
+  | Conj Formula Formula
+  | Impl Formula Formula
+
+
+type Signed a
+  = T a
+  | F a
+
+
+parseSigned = Parser.run (succeed identity |. spaces |= signedFormula |. spaces |. end)
+
+signedFormula =
+  succeed identity
+  |. spaces
+  |= oneOf
+    [ succeed T
+      |. keyword "T"
+      |. spaces
+      |= formula
+    , succeed F
+      |. keyword "F"
+      |. spaces
+      |= formula
+    ]
+
+-- parse = Parser.run formula
+parse = Parser.run (succeed identity |. spaces |= formula |. spaces |. end)
+
+formula : Parser Formula
+formula =
+  oneOf
+    [ succeed Atom
+        |= variable Char.isLower Char.isLower (Set.fromList [])
+    , succeed Neg
+        |. keyword "-"
+        |. spaces
+        |= lazy (\_ -> formula)
+    , lazy (\_ -> binary "&" Conj)
+    , lazy (\_ -> binary "|" Disj)
+    , lazy (\_ -> binary "->" Impl)
+    ]
+
+conjunction = lazy (\_ -> binary "&" Conj)
+disjunction = lazy (\_ -> binary "|" Conj)
+
+binary conn constructor =
+  delayedCommitMap constructor
+  (  succeed identity
+     |. symbol "("
+     |. spaces
+     |= lazy (\_ -> formula)
+     |. spaces
+  ) <| succeed identity
+  |. symbol conn
+  |. spaces
+  |= lazy (\_ -> formula)
+  |. spaces
+  |. symbol ")"
+
+
+spaces : Parser ()
+spaces =
+  ignore zeroOrMore (\char -> char == ' ')
