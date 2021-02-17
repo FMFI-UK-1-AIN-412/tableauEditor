4568eae90bbd4f07774fe29c5a6467fc1c1ff0aa
MatejKomlossy <matej.komlossy@gmail.com>
2021-02-17 14:59:45 +0100

    NCS validation
    Added missing imports

---
+++ src/Validation/Rules/NCS.elm
@@ -0,0 +1,76 @@
+module Validation.Rules.NCS exposing (..)
+
+import Formula exposing (Formula(..))
+import Formula.Signed exposing (Signed(..))
+import Tableau exposing (..)
+import Term exposing (Term(..))
+import Validation.Common exposing (..)
+import Zipper
+
+refStructureErr =
+    RuleError RefFormulasErr "NCS rule can't be used on referenced formulas"
+
+
+currentFormulaErr =
+    RuleError CurrentFormulaErr "formula was not created using the NCS rule"
+
+
+structureOne : Signed Formula -> Signed Formula -> Signed Formula -> Result RuleError String
+structureOne sf1 sf2 currentF =
+    case sf1 of
+        F (Conj a b) ->
+            case sf2 of
+                T f2 ->
+                    if a == f2 then
+                        case currentF of
+                            F cf ->
+                                if cf == b then
+                                    Ok "ok"
+
+                                else
+                                    Err currentFormulaErr
+
+                            _ ->
+                                Err currentFormulaErr
+
+                    else
+                        Err refStructureErr
+
+                _ ->
+                    Err refStructureErr
+
+        _ ->
+            Err refStructureErr
+
+
+structureTwo : Signed Formula -> Signed Formula -> Signed Formula -> Result RuleError String
+structureTwo sf1 sf2 currentF =
+    case sf1 of
+        F (Conj a b) ->
+            case sf2 of
+                T f2 ->
+                    if b == f2 then
+                        case currentF of
+                            F cf ->
+                                if cf == a then
+                                    Ok "ok"
+
+                                else
+                                    Err currentFormulaErr
+
+                            _ ->
+                                Err currentFormulaErr
+
+                    else
+                        Err refStructureErr
+
+                _ ->
+                    Err refStructureErr
+
+        _ ->
+            Err refStructureErr
+
+
+validate : Zipper.Zipper -> Result (List Problem) Zipper.Zipper
+validate z =
+    validate2RefUnaryRule "NCS" (tryBothOrdersAndStructures structureOne structureTwo) z
