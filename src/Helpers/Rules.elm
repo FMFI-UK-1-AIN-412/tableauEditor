386e14fdeaf1fbec52963cbd600d3d3cb52b7101
MatejKomlossy <matej.komlossy@gmail.com>
2021-02-25 20:23:07 +0100

    Update documentation about rules

--- src/Helpers/Rules.elm
+++ src/Helpers/Rules.elm
@@ -3,10 +3,11 @@ module Helpers.Rules exposing (..)
 import Dict
 import Formula exposing (Formula(..))
 import Formula.Signed exposing (Signed(..))
-import Html exposing (Attribute, Html, div, h2, h3, p, table, td, text, tr)
+import Html exposing (Attribute, Html, div, h2, h3, p, table, td, text, tr, sub, sup, span)
 import Html.Attributes exposing (..)
 import Markdown
 import Term exposing (Term(..))
+import Html exposing (code)
 
 
 fA =
@@ -48,7 +49,46 @@ deltas =
 gammas =
     [ T (ForAll "x" fD), F (Exists "x" fD) ]
 
+reflexivityFormulas = 
+    [("", "", "T t≐t")]
 
+mpFormulas = 
+    [("T (A→B)","T A", "T B")]
+
+mtFormulas = 
+    [("T (A→B)", "F B", "F A")]
+
+dsFormulas = 
+    [("T (A∨B)", "F A", "T B"), ("T (A∨B)", "F B", "T A")]
+
+ncsFormulas = 
+    [("F (A∧B)", "T A", "F B"), ("F (A∧B)", "T B", "F A")]
+
+cutFormulas = 
+    [("", "T A", "F A")]
+
+hsFormulas = 
+    [("T (A→B)", "T (B→C)", "T (A→C)")]
+
+esttFormulas = 
+    [("T (A↔B)", "T A", "T B"),("T (A↔B)", "T B", "T A")]
+
+estfFormulas = 
+    [("T (A↔B)", "F A", "F B"), ("T (A↔B)", "F B", "F A")]
+
+esftFormulas = 
+    [("F (A↔B)", "T A", "F B"), ("F (A↔B)", "T B", "F A")]
+
+esffFormulas = 
+    [("F (A↔B)", "F A", "T B"), ("F (A↔B)", "F B", "T A")]
+
+ecdtFormulas =
+    [("T (A↔B)", "T (A∧B)", "F (A∨B)")]
+
+ecdfFormulas =
+    [("F (A↔B)", "T (A∧¬B)", "F (A∨¬B)")]
+
+    
 renderAlpha : Signed Formula -> Html msg
 renderAlpha a =
     table [ class "rule" ] <|
@@ -98,6 +138,32 @@ renderDelta d =
                 (Formula.Signed.subformulas d)
 
 
+renderUnary : (String, String, String) -> Html msg
+renderUnary (f1, f2, f3) =
+    table [ class "rule", class "withWhiteSpace" ] <|
+        [ tr [] [ td [] [ text <| (f1 ++ "   " ++ f2) ] ]
+        , tr [] [ td [] [ text <| f3 ] ]
+        ]
+
+renderBinary : (String, String, String) -> Html msg
+renderBinary (f, sf1, sf2) =
+    table [class "rule"]
+        [ div [ class "formula" ]
+            [ text <| f
+            , div [ class "beta" ]
+                [ div [ class "formula" ] [ text <| sf1 ]
+                , div [ class "formula" ] [ text <| sf2 ]
+                ]
+            ]
+        ]
+
+renderGeneralGammaDelta (sign, quant) = 
+    table [ class "rule", class "withWhiteSpace" ] <|
+        [ tr [] [ td [] [ text (sign++" " ++ quant++"x") ,subs "1",text (",...,"++quant++"x") ,subs "n" ,text "A" ] ]
+        , tr [] [ td [] [ text (sign++" A{x"), subs "1", text "→y",subs "1",text",...,x",subs"n",text "→y",subs"n",text"}"] ]
+        ]
+
+
 demoSubst : String -> String -> Signed Formula -> Signed Formula
 demoSubst x y =
     signedMap
@@ -121,24 +187,29 @@ symbolsTable =
         [ h3 [] [ text "Propositional and first-order logical symbols" ]
         , Html.table [ class "rulesHelpTable" ]
             [ Html.tr []
-                [ Html.th [] [ text "Negation" ]
+                [ Html.th [] [ text "Negation" ] 
+                , Html.th [] [ text "Equality" ]
                 , Html.th [] [ text "Conjunction" ]
                 , Html.th [] [ text "Disjunction" ]
                 , Html.th [] [ text "Implication" ]
+                , Html.th [] [ text "Equivalence" ]
                 , Html.th [] [ text "Universal quantifier" ]
                 , Html.th [] [ text "Existential quantifier" ]
                 ]
             , Html.tr []
                 [ Html.td [] [ Markdown.toHtml [ class "symbols" ] "`-`, `~`, `¬`" ]
+                , Html.td [] [ Markdown.toHtml [ class "symbols" ] "`=`, `≐`" ]
                 , Html.td [] [ Markdown.toHtml [ class "symbols" ] "`&`, `/\\`, `∧`" ]
                 , Html.td [] [ Markdown.toHtml [ class "symbols" ] "`|`, `\\/`, `∨`" ]
                 , Html.td [] [ Markdown.toHtml [ class "symbols" ] "`->`, `→`" ]
+                , Html.td [] [ Markdown.toHtml [ class "symbols" ] "`<->`, `↔`" ]
                 , Html.td [] [ Markdown.toHtml [ class "symbols" ] "`∀`, `\\A`, `\\forall`, `\\a`" ]
                 , Html.td [] [ Markdown.toHtml [ class "symbols" ] "`∃`, `\\E`, `\\exists`, `\\e`" ]
                 ]
             , Html.tr []
                 [ Html.td [] [ text "unary" ]
-                , Html.td [ colspan 3 ]
+                , Html.td [] [ text "binary, takes two terms" ]
+                , Html.td [ colspan 4 ]
                     [ text "strictly binary, must be parenthesized" ]
                 , Html.td [ colspan 2 ]
                     [ text "takes a\u{00A0}variable and a formula" ]
@@ -203,6 +274,38 @@ notesTable =
         ]
 
 
+linearExample a b c = 
+    Html.td []
+        [ div [ class "formula" ]
+            [ text a
+            , div [ class "alpha" ]
+                [ div [ class "formula" ]
+                    [ text b
+                    , div [ class "alpha" ] [ div [ class "formula" ] [ text c ] ]
+                    ]
+                ]
+            ]
+        ]
+
+
+binaryExample a b c = 
+    Html.td []
+        [ div [ class "formula" ]
+            [ text a
+            , div [ class "beta" ]
+                [ div [ class "formula" ] [ text b ]
+                , div [ class "formula" ] [ text c ]
+                ]
+            ]
+        ]
+
+
+subs txt = 
+    sub[][text txt]
+
+sups txt = 
+    sup[][text txt]
+
 rulesTable =
     div [ class "full" ]
         [ h3 [] [ text "Applying rules" ]
@@ -213,6 +316,8 @@ rulesTable =
                 , Html.th [] [ text "β-rule" ]
                 , Html.th [] [ text "γ-rule" ]
                 , Html.th [] [ text "δ-rule" ]
+                , Html.th [] [ text "Reflexivity rule" ]
+                , Html.th [] [ text "Leibnitz rule" ]
                 ]
             , Html.tr []
                 [ Html.td [] [ text "rules" ]
@@ -220,32 +325,19 @@ rulesTable =
                 , Html.td [] [ div [] (List.map renderBeta betas) ]
                 , Html.td [] [ div [] (List.map renderGamma gammas) ]
                 , Html.td [] [ div [] (List.map renderDelta deltas) ]
+                , Html.td [] [ div [] (List.map renderUnary reflexivityFormulas) ]
+                , Html.td [] [table [ class "rule", class "withWhiteSpace" ] <|
+                    [ tr [] [ td [] [ text  "T t" , subs "1", text "≐t" , subs "2", text "  A", sups "+", text "{q→t", subs "1",text"}"]]
+                    , tr [] [td [][text "   A", sups"+", text "{q→t", subs "2",text"}"]  ]
+                 ]]
                 ]
             , Html.tr []
                 [ Html.td [] [ text "example" ]
+                , linearExample "(1) T(a∧b) [ ]" "(2) T a [1]" "(3) T b [1]"
+                , binaryExample "(1) T(a∨b) [ ]" "(2) T a [1]" "(3) T b [1]"
                 , Html.td []
                     [ div [ class "formula" ]
-                        [ text "(1) T(a∧b) [1]"
-                        , div [ class "alpha" ]
-                            [ div [ class "formula" ]
-                                [ text "(2) T a [1]"
-                                , div [ class "alpha" ] [ div [ class "formula" ] [ text "(3) T b [1]" ] ]
-                                ]
-                            ]
-                        ]
-                    ]
-                , Html.td []
-                    [ div [ class "formula" ]
-                        [ text "(1) T(a∨b) [1]"
-                        , div [ class "beta" ]
-                            [ div [ class "formula" ] [ text "(2) T a [1]" ]
-                            , div [ class "formula" ] [ text "(3) T b [1]" ]
-                            ]
-                        ]
-                    ]
-                , Html.td []
-                    [ div [ class "formula" ]
-                        [ text "(1) T ∀x P(x) [1]"
+                        [ text "(1) T ∀x P(x) [ ]"
                         , div [ class "gamma" ]
                             [ div [ class "formula" ]
                                 [ text "(2) T P(g(k,y)) {x→g(k,y)} [1]" ]
@@ -254,13 +346,108 @@ rulesTable =
                     ]
                 , Html.td []
                     [ div [ class "formula" ]
-                        [ text "(1) F ∀x P(x) [1]"
+                        [ text "(1) F ∀x P(x) [ ]"
                         , div [ class "delta" ]
                             [ div [ class "formula" ]
                                 [ text "(2) F P(z) {x→z} [1]" ]
                             ]
                         ]
                     ]
+                , linearExample "(1) T a≐a [ ]" "" ""
+                , linearExample "(1) T x≐f(y) [ ]" "(2) T p(x) [ ]" "(3) T p(f(y)) [1,2]"
+                ]
+            ]
+        ]
+
+
+rulesTable2 =
+    div [ class "full" ]
+        [ Html.table [ class "rulesHelpTable" ]
+            [ Html.tr []
+                [ Html.th [] [ text "" ]
+                , Html.th [] [ text "Cut rule" ]
+                , Html.th [] [ text "MP-rule (Modus Ponens)" ]
+                , Html.th [] [ text "MT-rule (Modus Tolens)" ]
+                , Html.th [] [ text "HS-rule" ]
+                , Html.th [] [ text "DS-rule" ]
+                , Html.th [] [ text "NCS-rule" ]
+                ]
+            , Html.tr []
+                [ Html.td [] [ text "rules" ]
+                , Html.td [] [ div [] (List.map renderBinary cutFormulas) ] 
+                , Html.td [] [ div [] (List.map renderUnary mpFormulas) ]
+                , Html.td [] [ div [] (List.map renderUnary mtFormulas) ]
+                , Html.td [] [ div [] (List.map renderUnary hsFormulas) ]
+                , Html.td [] [ div [] (List.map renderUnary dsFormulas) ]
+                , Html.td [] [ div [] (List.map renderUnary ncsFormulas) ]
+                ]
+            , Html.tr []
+                [ Html.td [] [ text "example" ]
+                , binaryExample "" "T a [ ]" "F a [ ]" 
+                , linearExample "(1) T(a→b) [ ]" "(2) T a [ ]" "(3) T b [1,2]"
+                , linearExample "(1) T(a→b) [ ]" "(2) F b [ ]" "(3) F a [1,2]"
+                , linearExample "(1) T(a→b) [ ]" "(2) T(b→c) [ ]" "(3) T(a→c) [1,2]"
+                , linearExample "(1) T(a∨b) [ ]" "(2) F a [ ]" "(3) T b [1,2]"
+                , linearExample "(1) F(a∧b) [ ]" "(2) T a [ ]" "(3) F b [1,2]"
+                ]
+            ]
+        ]
+
+
+rulesTable3 =
+    div [ class "full" ]
+        [ Html.table [ class "rulesHelpTable" ]
+            [ Html.tr []
+                [ Html.th [] [ text "" ]
+                
+                , Html.th [] [ text "ESTT-rule" ]
+                , Html.th [] [ text "ESTF-rule" ]
+                , Html.th [] [ text "ESFT-rule" ]
+                , Html.th [] [ text "ESFF-rule" ]
+                ]
+            , Html.tr [] 
+                [ Html.td [] [ text "rules" ]
+                
+                , Html.td [] [ div [] (List.map renderUnary esttFormulas) ]
+                , Html.td [] [ div [] (List.map renderUnary estfFormulas) ]
+                , Html.td [] [ div [] (List.map renderUnary esftFormulas) ]
+                , Html.td [] [ div [] (List.map renderUnary esffFormulas) ]
+                ]
+            , Html.tr []
+                [ Html.td [] [ text "example" ]
+                
+                , linearExample "(1) T(a↔b) [ ]" "(2) T b [ ]" "(3) T a [1,2]"
+                , linearExample "(1) T(a↔b) [ ]" "(2) F a [ ]" "(3) F b [1,2]"
+                , linearExample "(1) F(a↔b) [ ]" "(2) T a [ ]" "(3) F b [1,2]"
+                , linearExample "(1) F(a↔b) [ ]" "(2) F b [ ]" "(3) T a [1,2]"
+                ]
+            ]
+        ]
+
+
+rulesTable4 =
+    div [ class "full" ]
+        [ Html.table [ class "rulesHelpTable" ]
+            [ Html.tr []
+                [ Html.th [] [ text "" ]
+                , Html.th [] [ text "ECDT-rule" ]
+                , Html.th [] [ text "ECDF-rule" ]
+                , Html.th [] [ text "γ*-rule" ]
+                , Html.th [] [ text "δ*-rule" ]
+                ]
+            , Html.tr [] 
+                [ Html.td [] [ text "rules" ]
+                , Html.td [] [ div [] (List.map renderBinary ecdtFormulas) ]
+                , Html.td [] [ div [] (List.map renderBinary ecdfFormulas) ]
+                , Html.td [] [ div[] (List.map renderGeneralGammaDelta [("T", "∀"), ("F", "∃")]) ]
+                , Html.td [] [ div[] (List.map renderGeneralGammaDelta [("F", "∀"), ("T", "∃")]) ]
+                ]
+            , Html.tr []
+                [ Html.td [] [ text "example" ]
+                , binaryExample "(1) T(a↔b) [ ]" "(2) T(a∧b) [1]" "(3) F (a∨b) [1]"
+                , binaryExample "(1) F(a↔b) [ ]" "(2) F T (a∧¬b) [1]" "(3) F T (a∨¬b) [1]"
+                , linearExample "" "(1) (1) F ∀x∀y P(x,y) [ ] [ ]" "(2) F P(q,z) {x→q, y→z} [1]"
+                , linearExample "" "(1) T ∀x∀y P(x,y) [ ]" "(2) T P(g(k,z), q) {x→g(k,z) ,y→q} [1]"
                 ]
             ]
         ]
@@ -272,6 +459,9 @@ help =
         , symbolsTable
         , notesTable
         , rulesTable
+        , rulesTable2
+        , rulesTable3
+        , rulesTable4
         ]
 
 
