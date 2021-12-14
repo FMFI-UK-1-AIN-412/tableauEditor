46920e09cef3d018a854f812a050ce7c44ae6161
Matej Mok <matej.mok@gmail.com>
2021-12-14 21:27:04 +0100

    fixes

--- react-component-src/AppModule.jsx
+++ react-component-src/AppModule.jsx
@@ -1,16 +1,10 @@
 import React, { useEffect, useRef } from 'react';
+import ElmComponent from 'react-elm-components'
 const Elm = require('./elm-editor.js').Elm;
 
 function AppModule() {
-  const rootElement = useRef(null);
-  useEffect(() => {
-    Elm.Editor.init({
-      node: rootElement, 
-      flags: null 
-    });
-  })
   return (
-    <div ref={rootElement}></div>
+    <ElmComponent src={Elm.Editor} flags={null} />
   )
 }
 
