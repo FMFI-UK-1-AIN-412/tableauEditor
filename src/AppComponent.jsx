dd85409bd21bfb45eca15780602f315b544ac734
Ján Kľuka <jan.kluka@fmph.uniba.sk>
2023-03-12 22:03:29 +0100

    AppComponent: Backwards compatible state initialization

--- src/AppComponent.jsx
+++ src/AppComponent.jsx
@@ -1,20 +1,29 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import ElmComponent from 'react-elm-components'
 import './static/css/main.iso.css';
 import './static/css/all.iso.css';
 const Elm = require('./elm-editor.js').Elm;
 
 function prepare(initialState) {
+  if (typeof(initialState) == "string") {
+    // Backwards compatibility
+    // State is now a JSON value, but was stringified JSON before
+    try {
+      initialState = JSON.parse(initialState);
+    } catch (_) {
+      initialState = null;
+    }
+  }
   const instance = {
     initialState,
-    state: null,
+    state: initialState,
     triggerStateUpdate: null
   };
   const getState = (instance) => {
     if (instance.triggerStateUpdate) {
-      console.log("State before triggering update", instance.state);
+      // console.log("Triggering tableau editor state update");
       instance.triggerStateUpdate();
-      console.log("State after triggering update", instance.state);
+      // console.log("Triggered tableau editor state update");
     }
     return instance.state;
   }
@@ -28,9 +37,11 @@ function AppComponent(props) {
   const instance = props.instance;
   const setupPorts = (ports) => {
     ports.onChange.subscribe( () => {
+      // console.log("Tableau editor has changed");
       props.onStateChange();
     });
     ports.onStore.subscribe( (state) => {
+      // console.log("Tableau editor sent state to store");
       instance.state = state;
     })
     instance.triggerStateUpdate = () =>
